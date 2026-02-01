c5dcba85627e6f5e26efee1c9e0979f7c3c20d5a
usman <uu124@ic.ac.uk>
2026-02-01 00:00:45 +0000

    feat: Implement items endpoints [uu124]

---
+++ endpoints/items.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import get_db, crud
+from app import schemas
+
+router = APIRouter(prefix="/items", tags=["items"])
+
+@router.get("/{id}", response_model=schemas.ItemVariantRead)
+def get_item(id: str, db: Session = Depends(get_db)):
+    """Get details about an item."""
+    item = crud.get_item_variant(db, id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+@router.patch("/{id}", response_model=schemas.ItemVariantRead)
+def update_item(id: str, item_data: schemas.ItemVariantCreate, db: Session = Depends(get_db)):
+    """Change details about an item."""
+    updated_item = crud.update_item_variant(db, id, item_data)
+    if not updated_item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return updated_item
+
+@router.post("/", response_model=schemas.ItemVariantRead)
+def create_item(item_data: schemas.ItemVariantCreate, db: Session = Depends(get_db)):
+    """Create a new type of item. ID is assigned automatically by server."""
+    new_item = crud.create_item_variant(db, item_data)
+    return new_item
+    
