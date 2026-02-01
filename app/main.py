56265170c734f1277def49d203add524ed55aff2
Alex Hegedus-Adkin <alex@hegedusadkin.co.uk>
2026-02-01 04:16:04 +0000

    fix: fastapi docs straight up being wrong

--- app/main.py
+++ app/main.py
@@ -12,10 +12,12 @@ from .database import engine, Base
 
 app = FastAPI()
 
+permitted_origins = ["ichack.unigeorge.uk", "localhost"]
+
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=permitted_origins,  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
