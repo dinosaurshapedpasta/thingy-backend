d7339058ec9baf3adaa13642da4bde5970f591f8
Ismail Mehmood <ismail.mehmood24@imperial.ac.uk>
2026-02-01 04:16:28 +0000

    suicide

--- vrsp_solver_quantum.py
+++ vrsp_solver_quantum.py
@@ -21,9 +21,61 @@ def generate_data(seed):
     print(distance_matrix, drops_matrix, item_volumes, car_caps)
     return distance_matrix, drops_matrix, item_volumes, car_caps
 
-generate_data(42)
+distance_matrix, drops_matrix, item_volumes, car_caps = generate_data(42)
+
+def H_A(y, c, t, A):
+    N0, _, V = y.shape
+    
+    total = 0.0
+    for v in range(V):
+        for i in range(N0):
+            for j in range(N0):
+                if i != j:
+                    # cost for visiting i then j by same vehicle
+                    for alpha in range(N0 - 1):
+                        total += A * c[i, j, v] * y[i, alpha, v] * y[j, alpha + 1, v]
+            
+            # cost for departure from depot
+            for alpha in range(1, N0):
+                term1 = (1 - np.sum(y[:, alpha - 1, v]) + y[i, alpha - 1, v])
+                total += A * (t[v] + c[0, i, v]) * term1 * y[i, alpha, v]
+            
+            # cost for return to depot
+            for alpha in range(N0 - 1):
+                term2 = (1 - np.sum(y[:, alpha + 1, v]) + y[i, alpha + 1, v])
+                total += A * c[i, 0, v] * term2 * y[i, alpha, v]
+    
+    return total
+
+def H_B(y, B):
+    # Sum over cities and vehicles: exactly one visit
+    return B * np.sum((1 - np.sum(y, axis=(1, 2)))**2)
+
+def H_C(y, C):
+    # Sum over positions: unique assignment
+    return C * np.sum((1 - np.sum(y, axis=(0, 2)))**2)
 
 # 2. Formulate the Ising Hamiltonian
+def generate_hamiltonian(distance_matrix, drops_matrix, item_volumes, car_caps):
+    N0 = np.shape(drops_matrix)
+    V = np.shape(distance_matrix)
+    N = distance_matrix.shape[0]
+    # c[i, j, v]
+    c = np.zeros((N, N, V))
+    for v in range(V):
+        c[:, :, v] = distance_matrix
+    t = np.zeros(V) # atm i do not care
+
+    # Binary decision variables
+    # y[i, alpha, v] = 1 if vehicle v visits customer i in position alpha
+    y = np.zeros((N0, N0, V), dtype=int)  
+
+    # Hamiltonian weights
+    max_edge = np.max(distance_matrix)
+    A = 1.0
+    B = C = 10 * N0 * max_edge
+    h_a = H_A(y, c, t, A)
+    # h_b = H_B()
 
 
 # 3. Define a cost function in terms of the matrices
