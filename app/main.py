04199b1c68f679ac5835b84cd3cf856165e7d683
Alex Hegedus-Adkin <alex@hegedusadkin.co.uk>
2026-02-01 04:17:15 +0000

    fix: fastapi cors stuff again

--- app/main.py
+++ app/main.py
@@ -18,7 +18,6 @@ permitted_origins = ["ichack.unigeorge.uk", "localhost"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=permitted_origins,  # Allows all origins
-    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
